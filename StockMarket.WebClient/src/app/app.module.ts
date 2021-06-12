3262bd8069ae0c20234ddbbae2f9986c8dfb7792
Shridhar Hegde <shridhar.me.003@gmail.com>
2021-06-12 16:44:41 +0530

    Added material theme and routing

--- StockMarket.WebClient/src/app/app.module.ts
+++ StockMarket.WebClient/src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material.module";
+import { RoutingModule } from "./routing.module";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        RoutingModule,
+        MaterialModule
+    ],
+    declarations: [AppComponent, ToolbarComponent, LoginComponent],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
