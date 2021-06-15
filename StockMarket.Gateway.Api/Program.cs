56ac259ecf3701d5d4cf4fbbdb617812f86e1716
Shridhar Hegde <shridhar.me.003@gmail.com>
2021-06-15 19:00:52 +0530

    Added auth routing in gateway

--- StockMarket.Gateway.Api/Program.cs
+++ StockMarket.Gateway.Api/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace StockMarket.Gateway.Api
 {
@@ -16,11 +11,11 @@ namespace StockMarket.Gateway.Api
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((host, config) => config.AddJsonFile("ocelot.json"))
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+        }
     }
 }
